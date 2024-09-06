944236b5a055fb96603939d2fe0dd703a4c957c7
AngelGaarcia09 <a.22308051281193@cbtis128.edu.mx>
2024-09-06 09:32:50 -0600

    Add files via upload

---
+++ miscadenas.py
@@ -0,0 +1,23 @@
+"""Angel Garcia 
+trabajando con cadenas."""
+
+nombre="Angel"
+print(nombre [0])
+print(nombre) 
+
+MisNovias=["A","N","G"] 
+for chicas in MisNovias:
+ print(chicas)
+
+
+ print("otro ejemplo")
+ b = "Angel Garcia"
+print(b[2:5])  
+print("a mayuscula")
+a = "anegl garcia"
+print(a.upper()) 
+print("Concatenar") 
+a = "Maria"
+b = " Perez"
+c = a +" "+ b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,16 @@
+import math
+edad=16  # 16 se guarda en edad
+
+nombre="Angel Garcia" 
+print("tu edad es", edad)
+print("tu nombre es", nombre)
+print("-- tipo variables ---")
+x = str(7)    # x will be '3'
+y = int(3)    # y will be 3
+z=float(7)
+print(x)
+print(y)
+print(z)
+print("-- funciones matematicas---")
+x = math.sqrt(64) 
+print(x)
